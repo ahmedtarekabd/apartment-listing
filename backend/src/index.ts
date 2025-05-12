da89cbbccd342fb633ff35b30ec3efb8243ddf00
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2025-05-12 14:56:15 +0300

    BE: Enable CORS.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express'
 import routes from './routes/index'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const domain = process.env.DOMAIN || 'http://localhost'
 const port = process.env.PORT || 5000
+app.use(cors())
 
 app.use(express.json())
 app.use('/api/v1', routes)
